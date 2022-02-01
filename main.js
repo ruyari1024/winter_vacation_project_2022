2ff4b80036db2c370b1ccf77f0e8a369d4899ed7
ruyari1024 <ruyari1024@gmail.com>
2022-02-01 23:08:01 +0900

    기능 구현 전

---
+++ main.js
@@ -0,0 +1,28 @@
+let canvas = document.getElementsByClassName('canvas')
+let drag = false;
+const ctx = canvas.getContext('2d');
+
+document.ondrag = function (event) {
+    document.getElementById("demo").innerHTML
+                = "Element is being dragged";
+};
+
+document.onclick = function (event) {
+    document.getElementById("demo").innerHTML
+            = "Element is being clicked!";
+};
+ctx.lineWidth = 10;
+
+// Wall
+ctx.strokeRect(75, 140, 150, 110);
+
+// Door
+ctx.fillRect(130, 190, 40, 60);
+
+// Roof
+ctx.beginPath();
+ctx.moveTo(50, 140);
+ctx.lineTo(150, 60);
+ctx.lineTo(250, 140);
+ctx.closePath();
+ctx.stroke();
